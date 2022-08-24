eedd3c067ae9f184d59a4ea76411ffe3a9e39955
Nima Arefi <25016067+nimaa77@users.noreply.github.com>
2022-08-24 12:40:56 +0430

    feat: update the example

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-import express from 'express';
+import express from "express";
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 
 // disable cache
-app.set('etag', false);
+app.set("etag", false);
 
 app.use((_req, res, next) => {
-  res.set('Cache-Control', 'no-store');
+  res.set("Cache-Control", "no-store");
   next();
 });
 
-app.get('/backend', (req, res) => {
-  const cookies = req.headers.cookie || '';
-  console.log(cookies.split(';'));
+app.get("/set-cookie", (_req, res) => {
+  res.cookie("token", "superSecureToken", {
+    httpOnly: true,
+  });
   res.end();
 });
 
-app.get('/', (_req, res) => {
-  res.sendFile('index.html', { root: './public' });
+app.get("/backend", (req, res) => {
+  const cookies = req.headers.cookie || "";
+  console.log(cookies.split(";"));
+  res.end();
+});
+
+app.get("/", (_req, res) => {
+  res.sendFile("index.html", { root: "./public" });
 });
 
 app.listen(PORT, () => {
